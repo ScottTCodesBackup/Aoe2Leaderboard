09efcf7e14e23c2015460914f0c3fd8c77e49086
Scott Taylor <scott1996taylor@outlook.com>
2019-10-21 18:02:46 +0100

    Wrapped everything in the export

--- studio/functions/update-sanity-content.js
+++ studio/functions/update-sanity-content.js
@@ -1,53 +1,46 @@
 const sanityClient = require('@sanity/client')
 
-const client = sanityClient({
-  projectId: 'v7sgze3m',
-  dataset: 'production',
-  token: process.env.SANITY_TOKEN,
-  useCdn: false
-})
-
-const getExpected = (a, b) => {
-  return 1 / (1 + Math.pow(10, (b - a) / 400))
-}
-
-const onevone = players => {
-  const matchData = {}
-  const player1 = players[0]
-  const player2 = players[1]
-
-  const updateRating = (expected, actual, current) => {
-    const newRating = Math.round(current + 32 * (actual - expected))
-    return newRating
-  }
-
-  matchData[player1.ref._ref] = {rank: player1.rank, difference: 0, name: player1.name}
-  matchData[player2.ref._ref] = {rank: player2.rank, difference: 0, name: player2.name}
-
-  const player1Expected = getExpected(player1.rank, player2.rank)
-  const player2Expected = getExpected(player2.rank, player1.rank)
-
-  let player1NewRank
-  let player2NewRank
-
-  if (player1.score > player2) {
-    player1NewRank = updateRating(player1Expected, 1, player1.rank)
-    player2NewRank = updateRating(player2Expected, 0, player2.rank)
-  } else {
-    player1NewRank = updateRating(player1Expected, 0, player1.rank)
-    player2NewRank = updateRating(player2Expected, 1, player2.rank)
-  }
-
-  matchData[player1.ref._ref].newRank = player1NewRank
-  matchData[player2.ref._ref].newRank = player2NewRank
-
-  matchData[player1.ref._ref].difference = player1NewRank - player1.rank
-  matchData[player2.ref._ref].difference = player2NewRank - player2.rank
-
-  return matchData
-}
-
 exports.handler = async (event, context) => {
+  const getExpected = (a, b) => {
+    return 1 / (1 + Math.pow(10, (b - a) / 400))
+  }
+
+  const onevone = players => {
+    const matchData = {}
+    const player1 = players[0]
+    const player2 = players[1]
+
+    const updateRating = (expected, actual, current) => {
+      const newRating = Math.round(current + 32 * (actual - expected))
+      return newRating
+    }
+
+    matchData[player1.ref._ref] = {rank: player1.rank, difference: 0, name: player1.name}
+    matchData[player2.ref._ref] = {rank: player2.rank, difference: 0, name: player2.name}
+
+    const player1Expected = getExpected(player1.rank, player2.rank)
+    const player2Expected = getExpected(player2.rank, player1.rank)
+
+    let player1NewRank
+    let player2NewRank
+
+    if (player1.score > player2) {
+      player1NewRank = updateRating(player1Expected, 1, player1.rank)
+      player2NewRank = updateRating(player2Expected, 0, player2.rank)
+    } else {
+      player1NewRank = updateRating(player1Expected, 0, player1.rank)
+      player2NewRank = updateRating(player2Expected, 1, player2.rank)
+    }
+
+    matchData[player1.ref._ref].newRank = player1NewRank
+    matchData[player2.ref._ref].newRank = player2NewRank
+
+    matchData[player1.ref._ref].difference = player1NewRank - player1.rank
+    matchData[player2.ref._ref].difference = player2NewRank - player2.rank
+
+    return matchData
+  }
+
   const {body} = event
   if (body) {
     const bodyParsed = JSON.parse(body)
@@ -55,6 +48,13 @@ exports.handler = async (event, context) => {
     if (!bodyParsed.ids || bodyParsed.ids.created.length === 0) {
       return {statusCode: 200}
     }
+
+    const client = sanityClient({
+      projectId: 'v7sgze3m',
+      dataset: 'production',
+      token: process.env.SANITY_TOKEN,
+      useCdn: false
+    })
 
     try {
       const res = await bodyParsed.ids.created.map(_id => {
