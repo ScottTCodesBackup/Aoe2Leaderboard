f0f6e52900b344f397e90d694cee4791bc65c09c
Scott Taylor <scott1996taylor@outlook.com>
2019-10-21 17:09:30 +0100

    Fixed console log error

--- studio/functions/update-sanity-content.js
+++ studio/functions/update-sanity-content.js
@@ -50,7 +50,7 @@ const onevone = players => {
 exports.handler = async (event, context) => {
   const {body} = event
 
-  console.log(`Event body exists: ${event.body ? 'true' : 'false'}, IDS exists: ${event.body.ids ? 'true' : 'false'}, Created exists: ${event.body.ids.created.length > 0 ? 'true' : 'false'}`)
+  console.log(`Event body exists: ${event && event.body ? 'true' : 'false'}, IDS exists: ${event && event.body && event.body.ids ? 'true' : 'false'}, Created exists: ${event && event.body && event.body.ids && event.body.ids.created.length > 0 ? 'true' : 'false'}`)
   if (!event.body || !event.body.ids || event.body.ids.created.length > 0) {
     return {statusCode: 200}
   }
